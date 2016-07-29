f5a08fdc16da46f870c8fa9a23440fe9dc60fbb0
2358 <2358@km-10051.mopr.lan>
2016-07-29 10:59:13 +0200

    Trening obiektowwości - interfejsy

--- index.php
+++ index.php
@@ -28,8 +28,10 @@ and open the template in the editor.
 
             private $_observers = array();
 
-            public function add(Observer $observer) {
-                $this->_observers[] = $observer;
+            public function add(Observer $observer) {               
+                foreach ($observer as $key => $val) {
+                    $this->_observers[] = $val;
+                }              
             }
 
             public function remove(Observer $observer) {
@@ -37,7 +39,15 @@ and open the template in the editor.
                     unset($this->_observers[$key]);
                 }
             }
-
+            
+            /**
+             * 
+             * @return type array obj
+             */
+            protected function getObservers() {
+                return $this->_observers;
+            }
+            
             public function notifyAll() {
                 echo '<br />z interfejsu';
             }
@@ -59,24 +69,28 @@ and open the template in the editor.
 
             private $_observer;
             
-            function __construct(Observer $observer) {
-                $this->_observer = $observer;
-            }
+//            function __construct(Observer $observer) {
+//                $this->_observer = $observer;
+//            }
 
             public function test() {
-                echo '<br /> zwykła klasa ';
-                $this->notifyAll();
-                $this->chroniona();
-                echo "<br /><br /><br /> obiekt Observer" . var_dump($this->_observer) . '<br /><br />'; 
-                //parent::notifyAll();
-                //$this->add($this->_observer);
+//                echo '<br /> zwykła klasa ';
+//                $this->notifyAll();
+//                $this->chroniona();
+////                echo "<br /> obiekt Observer" . print_r(get_class_methods($this->_observer));
+//                parent::notifyAll();
+//                $this->add($this->_observer);
+                print_r($this->getObservers());
             }
 
         }
         
-        $testy = new testowa(new Observer('Piotr Kaczorowski'));
+        $testy = new testowa();
+        $testy->add(new Observer('Piotr Kaczorowski'));
+        $testy->add(new Observer('Jan Kaczorowski'));
+        
         $testy->test();
-        $testy->notifyAll();
+//        $testy->notifyAll();
         ?>
     </body>
 </html>
