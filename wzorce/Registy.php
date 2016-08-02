e588282a7ef4ccd70a109b55d1273685abd1f6ce
2358 <2358@km-10051.mopr.lan>
2016-08-02 12:38:59 +0200

    wzorce  - rejest

---
+++ wzorce/Registy.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of rejest
+ *
+ * @author 2358
+ */
+
+interface RegistyInterface {
+    public static function get();
+    public static function set();
+    public static function contains();
+    public static function remove();
+}
+
+class Registy implements RegistyInterface{
+    
+    public static function get(){
+        
+    }
+    public static function set(){
+        
+    }
+    public static function contains(){
+        
+    }
+    public static function remove(){
+        
+    }
+}
