e31ababdf7ae065493703ac01dc7ec5345f1d15d
2358 <2358@km-10051.mopr.lan>
2016-08-03 08:46:30 +0200

    wzorce  -  index do testów

--- wzorce/Singleton.php
+++ wzorce/Singleton.php
@@ -32,18 +32,7 @@ trait Singleton { //extends PDO
 
 
 class DBWrite extends PDO {
-//    private static $_instance = null;
-//
-//    public static function getInstance() {
-//
-//        $class = __CLASS__;
-//        
-//        if(!(self::$_instance instanceof $class)) {
-//            self::$_instance = new $class();
-//        }
-//        return self::$_instance;
-//    }
-   
+
     use Singleton;
   
     public function __construct() {
@@ -51,5 +40,11 @@ class DBWrite extends PDO {
     }
 }
 
-$obj = DBWrite::getInstance();
-var_dump($obj);
+class DBRead extends PDO {
+
+    use Singleton;
+  
+    public function __construct() {
+        parent::__construct('mysql:dbname=shellholidayrwd;host=127.0.0.1','root','');
+    }
+}
