df1c5d2c9bce97d2ed013c54c07746af3a7ade8a
2358 <2358@km-10051.mopr.lan>
2016-08-03 08:42:51 +0200

    wzorce  -  index do testów

--- wzorce/Singleton.php
+++ wzorce/Singleton.php
@@ -1,5 +1,8 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -7,10 +10,46 @@
  */
 
 /**
- * Description of Singleton
+ * Description of singleton
  *
  * @author 2358
  */
-class Singleton {
-    //put your code here
+trait Singleton { //extends PDO 
+    
+    private static $_instance = null;
+
+    public static function getInstance() {
+
+        $class = __CLASS__;
+        
+        if(!(self::$_instance instanceof $class)) {
+            self::$_instance = new $class();
+        }
+        return self::$_instance;
+    }
+
 }
+
+
+class DBWrite extends PDO {
+//    private static $_instance = null;
+//
+//    public static function getInstance() {
+//
+//        $class = __CLASS__;
+//        
+//        if(!(self::$_instance instanceof $class)) {
+//            self::$_instance = new $class();
+//        }
+//        return self::$_instance;
+//    }
+   
+    use Singleton;
+  
+    public function __construct() {
+        parent::__construct('mysql:dbname=shellholiday;host=127.0.0.1','root','');
+    }
+}
+
+$obj = DBWrite::getInstance();
+var_dump($obj);
