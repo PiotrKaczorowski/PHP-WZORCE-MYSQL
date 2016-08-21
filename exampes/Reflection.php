dcf517d2468fda738b48e3620d4000d8839c75ca
Brzeźno Piotr Kaczorowski <office@ctechnology.pl>
2016-08-21 19:09:53 +0200

    Reflection

---
+++ exampes/Reflection.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Reflection
+ *
+ * @author 2358
+ */
+class Test {
+    
+    private $foo = '';
+    private $bar = '';
+
+    public __construct() {
+        
+    }
+    
+    public function ecboFoo() {
+        
+    }
+    public function echoBar() {
+        
+    }
+}
